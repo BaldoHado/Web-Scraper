4dcf6df2432499edde226b08cfe785b8c8f42435
Adam Ta <adamta2004@gmail.com>
2023-02-22 17:50:13 -0800

    Changed UI to Tkinter - Grid

--- user_interface.py
+++ user_interface.py
@@ -4,21 +4,20 @@ import summary_scrap as ss
 
 
 def submit():
-    stock_name = input_stock.get()
+    stock_name = inp_stock.get()
     print(stock_name)
     stat_header = tk.Label(text=f'Summary of the Statistics of {stock_name}')
-    stat_header.pack()
-
+    stat_header.grid(row=1, column=0, pady=2, padx=2, columnspan=2)
 
 
 # create the main window
 root = tk.Tk()
 root.title('Stock Viewer')
 root.geometry('600x300')
-label = tk.Label(text='Enter a Value')
-input_stock = tk.Entry()
+en_val = tk.Label(text='Enter a Value')
+inp_stock = tk.Entry()
 tree = ttk.Treeview(root, columns=("Header", "Value"), show='headings')
-enter = tk.Button(root, text='Submit', command=submit)
+bt_submit = tk.Button(root, text='Submit', command=submit)
 
 # Column Headings
 tree.heading("Header", text="Header")
@@ -30,6 +29,10 @@ for k in ins:
     tree.insert('', tk.END, values=k)
 
 # Packing Widgets
+en_val.grid(row=0, column=0, pady=2)
+inp_stock.grid(row=0, column=1, pady=2)
+bt_submit.grid(row=0, column=2, pady=2, padx=2)
+tree.grid(row=2, column=0, rowspan=5, columnspan=5)
 # label.pack()
 # input_stock.pack(pady=10)
 # enter.pack()
